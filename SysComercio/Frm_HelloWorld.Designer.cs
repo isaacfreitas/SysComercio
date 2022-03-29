e34294fc5ceec6f309a0b36624eb800d358e2838
isaacfreitas <isaacfrancafreitas@outlook.com>
2022-03-29 15:22:34 -0300

    pequenas alterações no windows forms para o Hello World

---
+++ SysComercio/Frm_HelloWorld.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace SysComercio
+{
+    partial class Frm_HelloWorld
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Titulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_Titulo
+            // 
+            this.lbl_Titulo.AutoSize = true;
+            this.lbl_Titulo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Titulo.Location = new System.Drawing.Point(30, 30);
+            this.lbl_Titulo.Name = "lbl_Titulo";
+            this.lbl_Titulo.Size = new System.Drawing.Size(196, 18);
+            this.lbl_Titulo.TabIndex = 0;
+            this.lbl_Titulo.Text = "Visual Studio .NET Version";
+            this.lbl_Titulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Frm_HelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_Titulo);
+            this.Name = "Frm_HelloWorld";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hello World";
+            this.Load += new System.EventHandler(this.HelloWorld_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lbl_Titulo;
+    }
+}
---
+++ SysComercio/Frm_HelloWorld.cs
@@ -0,0 +1,20 @@
+namespace SysComercio
+{
+    public partial class Frm_HelloWorld : Form
+    {
+        public Frm_HelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloWorld_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
